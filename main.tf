f3d2a59d8406f29680af238145ff0522455f57c0
paola-perezrivera <paolaalexandra729@gmail.com>
2024-03-03 22:12:53 -0600

    testing terraform pipeline

--- main.tf
+++ main.tf
@@ -1,47 +1,64 @@
-terraform {
-  cloud {
-    organization = "morningstar_tech_challenge"
+# terraform {
+#   cloud {
+#     organization = "morningstar_tech_challenge"
 
-    workspaces {
-      name = "tech_challenge"
-    }
-  }
-}
+#     workspaces {
+#       name = "tech_challenge"
+#     }
+#   }
+# }
 
-## need to create the vpc, subnets, internet gateway
-
-################
-## AWS VPC
-resource "aws_vpc" "main" {
-    cidr_block           = var.vpc_cidr
-    enable_dns_hostnames = true
-    tags = {
-        name = "main"
+# ################
+# ## AWS VPC
+# resource "aws_vpc" "main" {
+#     cidr_block           = var.vpc_cidr
+#     enable_dns_hostnames = true
+#     tags = {
+#         name = "main"
         
+#     }
+# }
+
+# resource "aws_subnet" "subnet_1" {
+#     vpc_id                  = aws_vpc.main.id
+#     cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 16, 1)
+#     map_public_ip_on_launch = true
+#     availability_zone       = var.first_availability_zone
+
+#     depends_on = [ aws_vpc.main ]
+# }
+
+# resource "aws_subnet" "subnet_2" {
+#     vpc_id                  = aws_vpc.main.id
+#     cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 16, 2)
+#     map_public_ip_on_launch = true
+#     availability_zone       = var.second_availability_zone
+
+#     depends_on = [ aws_vpc.main ]
+# }
+
+# resource "aws_internet_gateway" "internet_gateway" {
+#     vpc_id = aws_vpc.main.id
+#     tags = {
+#         name = "internet_gateway"
+#     }
+# }
+
+    terraform {
+      backend "remote" {
+        # The name of your Terraform Cloud organization.
+        organization = "example-organization"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+          name = "example-workspace"
+        }
+      }
     }
-}
 
-resource "aws_subnet" "subnet" {
-    vpc_id                  = aws_vpc.main.id
-    cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 16, 1)
-    map_public_ip_on_launch = true
-    availability_zone       = var.first_availability_zone
-
-    depends_on = [ aws_vpc.main ]
-}
-
-resource "aws_subnet" "subnet" {
-    vpc_id                  = aws_vpc.main.id
-    cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 16, 2)
-    map_public_ip_on_launch = true
-    availability_zone       = var.second_availability_zone
-
-    depends_on = [ aws_vpc.main ]
-}
-
-resource "aws_internet_gateway" "internet_gateway" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        name = "internet_gateway"
+    # An example resource that does nothing.
+    resource "null_resource" "example" {
+      triggers = {
+        value = "A example resource that does nothing!"
+      }
     }
-}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
-variable vpc_cidr{default = "10.0.0.0/16"}
-variable first_availability_zone{ default = "us-east-2a"}
-variable second_availability_zone{ default = "us-east-2b"}
+variable vpc_cidr{
+    description = "CIDR block for VPC"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable first_availability_zone{ 
+    description = "One of the two availability zones in us-east-2"
+    type = string
+    default = "us-east-2a"
+}
+
+variable second_availability_zone{ 
+    description = "One of the two availability zones in us-east-2"
+    type = string
+    default = "us-east-2b"
+}
